2ca824d1334f5cdb4300a07775c9027a4368f5f5
xushouxing <591139906@qq.com>
2019-10-19 23:57:36 +0800

    删除了

--- elasticse-search/src/main/java/cn/itcast/repository/ItemRepository.java
+++ elasticse-search/src/main/java/cn/itcast/repository/ItemRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 
 public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
     List<Item> findByTitleAndPriceBefore(String s, double d1, double d2);
-    List<Item> findByPriceBetween(double price1, double price2);
 }
